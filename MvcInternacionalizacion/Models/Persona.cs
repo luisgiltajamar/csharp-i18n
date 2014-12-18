b19eaf2edde06b2c04d99578a7887cd742c92242
luisgiltajamar <luis.gil@tajamar365.com>
2014-12-18 10:08:11 +0100

    a

---
+++ MvcInternacionalizacion/Models/Persona.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcInternacionalizacion.Models
+{
+    public class Persona
+    {
+        [Required]
+        [DisplayName("Nombre")]
+        public String Nombre { get; set; }
+        
+       [Range(18,65)]
+        [DisplayName("Edad")]
+        public int Edad { get; set; }
+    }
+}
